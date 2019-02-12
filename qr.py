8f731cde73d5694458bc10a55dfad22363806a74
raspi-JM <woalsdl500@naver.com>
2019-02-12 15:55:48 +0900

    UPDATE qr.py

--- qr.py
+++ qr.py
@@ -10,6 +10,14 @@ while(True):
 	for barcode in barcodes:
 		(x,y,w,h) = barcode.rect
 		cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+		
+		barcodeData = barcode.data.decode("utf-8")
+		barcodeType = barcode.type
+
+		text = "["+barcodeType+"]( value = " + barcodeData + ")"
+		cv2.putText(frame,text,(x+w/2,y),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,255),2)
+
+		print("[INFO] Found {} barcode: {}".format(barcodeType, barcodeData))
 
 	cv2.imshow('image',frame)
 	
